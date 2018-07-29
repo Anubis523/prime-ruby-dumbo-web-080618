b59c597767112e93b865619bdaffbf03d1385ba8
Kevin Steele <kevindsteeleii@gmail.com>
2018-07-29 12:04:46 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,8 @@
 # Add  code here!
 def prime? (num)
-  if num == 0 or num == 1 or num 
+  if num == 0 or num == 1 or num < 0 
+    false
+  end
   (2..Math.sqrt(num).ceil).each{|i|
     if (num % i == 0 || num < 0)
       false 
